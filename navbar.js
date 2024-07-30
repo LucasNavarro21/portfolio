92f13db9785c58ae57154ffcb04dfbd5e0b62a00
Lucas <navarrolucas4668@gmail.com>
2024-07-30 07:46:45 -0300

    ingrese nuevas paginas y tambien las hice responsivas para tablets

---
+++ navbar.js
@@ -0,0 +1,13 @@
+// navbar.js
+export function loadNavbar() {
+    return `
+       <nav>
+        <ul class="contenedorNavbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <!-- <li><a href="#services">Services</a></li> -->
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+    `;
+}
