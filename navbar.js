18a71e7ef57fb756a56038899fd6203e59380a5f
Lucas <navarrolucas4668@gmail.com>
2024-07-30 09:00:38 -0300

    hice el diseño para celulares

--- navbar.js
+++ navbar.js
@@ -3,7 +3,7 @@ export function loadNavbar() {
     return `
        <nav>
         <ul class="contenedorNavbar">
-            <li><a href="#home">Home</a></li>
+            <li><a href="../index.html">Home</a></li>
             <li><a href="#about">About</a></li>
             <!-- <li><a href="#services">Services</a></li> -->
             <li><a href="#contact">Contact</a></li>
